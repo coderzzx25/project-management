62a85ec23b584b1a7255668afcae1109a05d9328
coderzzx <coderzzx@coderzzxdeMacBook-Air.local>
2023-12-31 11:07:28 +0800

    Mod -> app -> 自动调整暗黑模式

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,40 @@
-import { memo } from 'react';
+import { memo, useEffect, useMemo } from 'react';
 import { FC, ReactNode } from 'react';
 import { ConfigProvider, theme } from 'antd';
 import zhCN from 'antd/es/locale/zh_CN';
 
 import Router from '@/router';
-import { useAppSelector, useAppShallowEqual } from './store';
+import { useAppDispatch, useAppSelector, useAppShallowEqual } from './store';
+import { changeIsDarkReducer } from './store/modules/main';
 
 interface IProps {
   children?: ReactNode;
 }
 
 const App: FC<IProps> = () => {
+  const dispatch = useAppDispatch();
   const { isDark, themeColor } = useAppSelector((state) => state.main, useAppShallowEqual);
   const {
     token: { colorBgContainer }
   } = theme.useToken();
+  /**
+   * 根据当前系统时间判断是否为夜间模式
+   * 18:00 ~ 06:00 为夜间模式
+   * 06:00 ~ 18:00 为白天模式
+   * @returns boolean
+   */
+  const onDark = useMemo(() => {
+    const hour = new Date().getHours();
+    return hour >= 18 || hour < 6;
+  }, []);
+
+  useEffect(() => {
+    if (onDark) {
+      dispatch(changeIsDarkReducer(true));
+    } else {
+      dispatch(changeIsDarkReducer(false));
+    }
+  }, [onDark]);
   return (
     <ConfigProvider
       theme={{
